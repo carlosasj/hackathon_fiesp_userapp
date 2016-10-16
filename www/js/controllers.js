c258eeec298eda4bdb743d3fbe9e60f5625c6f09
Carlos Schneider <carlosaschjr@gmail.com>
2016-10-15 21:52:25 -0300

    Remove dependência que entrou junto com o projeto; Adiciona Autocomplete de endereços do Google

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,23 @@ angular.module('starter.controllers', [])
 
 .controller('TrackCtrl', function($scope) {})
 
-.controller('RoutesCtrl', function($scope, Chats) {
+.controller('RoutesCtrl', function($scope, $q) {
+
+  var geocoder = new google.maps.Geocoder();
+
+  $scope.getAddressSuggestions = function(queryString){
+    var defer = $q.defer();
+    geocoder.geocode(
+        {address: queryString},
+        function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) { defer.resolve(results); }
+            else { defer.reject(results); }
+        }
+    );
+    return defer.promise;
+  };
+
+
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
